87456d5506965a3f332c42805c1a736212678128
Ahmed <ahmed.belghith@medtech.tn>
2022-05-14 11:53:02 +0100

    first commit

---
+++ CMakeFiles/flying-plane-sdl-animation.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/animation.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/animation.c.obj"
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/background.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/background.c.obj"
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/car.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/car.c.obj"
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/car2.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/car2.c.obj"
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/coin.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/coin.c.obj"
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/image.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/image.c.obj"
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/main.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/main.c.obj"
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/src/taxi.c" "C:/Users/abelg/Documents/build-flying-plane-sdl-animation-master-MinGW-Debug/CMakeFiles/flying-plane-sdl-animation.dir/src/taxi.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/Users/abelg/Documents/flying-plane-sdl-animation-master/include"
+  "C:/SDL2-2.0.10/i686-w64-mingw32/include/SDL2"
+  "C:/SDL2_image-2.0.5/i686-w64-mingw32/include/SDL2"
+  "C:/SDL2_gfx-1.0.4/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/flying-plane-sdl-animation.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/animation.c.obj"
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/background.c.obj"
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/car.c.obj"
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/car2.c.obj"
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/coin.c.obj"
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/image.c.obj"
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/main.c.obj"
+  "CMakeFiles/flying-plane-sdl-animation.dir/src/taxi.c.obj"
+  "flying-plane-sdl-animation.exe"
+  "flying-plane-sdl-animation.exe.manifest"
+  "flying-plane-sdl-animation.pdb"
+  "libflying-plane-sdl-animation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/flying-plane-sdl-animation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
